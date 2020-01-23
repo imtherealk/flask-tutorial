b51b9641296c675ca9ea9ee7f130f4865545abd9
Nayeong <imtherealk@gmail.com>
2020-01-23 10:08:33 +0900

    Add init-db command

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+import click
 from flask import g, current_app
+from flask.cli import with_appcontext
 
 
 def get_db():
@@ -18,3 +20,17 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
