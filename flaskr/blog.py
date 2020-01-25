d81998da3957321870cb8655f1b09d6bf48902b4
Nayeong <imtherealk@gmail.com>
2020-01-25 16:52:39 +0900

    Update blog.py: Create 'get_post' function

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, g
+from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
@@ -41,3 +42,20 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
