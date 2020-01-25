033e4c6ea7775671a9ea43b5de13fdbb6e448ff9
Nayeong <imtherealk@gmail.com>
2020-01-25 15:57:54 +0900

    Create 'load_logged_in_user' function

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for, session
+from flask import Blueprint, flash, redirect, render_template, request, url_for, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from flaskr.db import get_db
@@ -60,3 +60,15 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
